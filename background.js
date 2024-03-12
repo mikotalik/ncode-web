4e5409943081da97ab00d5b39aaba4f6a267d319
Miroslav Kotalik <mikotalik@seznam.cz>
2024-03-12 04:10:34 +0100

    Epic background, swipe gestures, mobile optimization, advance arrow

---
+++ background.js
@@ -0,0 +1,145 @@
+const canvas = document.getElementById("background-canvas")
+
+
+let field = []
+let rez = 100
+let cols, rows
+let increment = 0.2
+let zoff = 0
+let noise
+
+let pointerX = 0
+let pointerY = 0
+
+let gridNoise
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight, canvas)
+    pixelDensity(0.7);
+    frameRate(30)
+
+    noise = new OpenSimplexNoise(Date.now())
+    grid = new OpenSimplexNoise(Date.now())
+
+    cols = 1 + width / rez
+    rows = 1 + height / rez
+    for (let i = 0; i < cols; i++) {
+        let k = []
+        for (let j = 0; j < rows; j++) {
+            k.push(0)
+        }
+        field.push(k)
+    }
+}
+function draw() {
+    background(0)
+
+    pointerX += (mouseX - pointerX) * 0.2
+    pointerY += (mouseY - pointerY) * 0.2
+
+    let xoff = 0
+    for (let i = 0; i < cols; i++) {
+        xoff += increment
+        let yoff = 0
+        for (let j = 0; j < rows; j++) {
+            field[i][j] = float(noise.noise3D(xoff, yoff, zoff))
+            field[i][j] *= dist(pointerX / rez, pointerY / rez, i, j)
+
+            yoff += increment
+        }
+    }
+    zoff += 0.01
+
+
+    for (let h = -1; h < 1; h += 0.25) {
+        stroke(255 * h, 255 * -h, 127)
+        strokeWeight(8)
+
+        for (let i = 0; i < cols - 1; i++) {
+            for (let j = 0; j < rows - 1; j++) {
+
+                let f0 = field[i][j] - h
+                let f1 = field[i + 1][j] - h
+                let f2 = field[i + 1][j + 1] - h
+                let f3 = field[i][j + 1] - h
+
+                let x = i * rez
+                let y = j * rez
+                let a = createVector(x + rez * f0 / (f0 - f1), y)
+                let b = createVector(x + rez, y + rez * f1 / (f1 - f2))
+                let c = createVector(x + rez * (1 - f2 / (f2 - f3)), y + rez)
+                let d = createVector(x, y + rez * (1 - f3 / (f3 - f0)))
+
+                let state = getState(f0, f1, f2, f3)
+                switch (state) {
+                    case 1:
+                        drawLine(c, d)
+                        break
+                    case 2:
+                        drawLine(b, c)
+                        break
+                    case 3:
+                        drawLine(b, d)
+                        break
+                    case 4:
+                        drawLine(a, b)
+                        break
+                    case 5:
+                        drawLine(a, d)
+                        drawLine(b, c)
+                        break
+                    case 6:
+                        drawLine(a, c)
+                        break
+                    case 7:
+                        drawLine(a, d)
+                        break
+                    case 8:
+                        drawLine(a, d)
+                        break
+                    case 9:
+                        drawLine(a, c)
+                        break
+                    case 10:
+                        drawLine(a, b)
+                        drawLine(c, d)
+                        break
+                    case 11:
+                        drawLine(a, b)
+                        break
+                    case 12:
+                        drawLine(b, d)
+                        break
+                    case 13:
+                        drawLine(b, c)
+                        break
+                    case 14:
+                        drawLine(c, d)
+                        break
+                }
+            }
+        }
+    }
+
+}
+function getState(a, b, c, d) {
+    return (a > 0 ? 8 : 0) + (b > 0 ? 4 : 0) + (c > 0 ? 2 : 0) + (d > 0 ? 1 : 0)
+}
+
+function drawLine(v1, v2) {
+    line(v1.x, v1.y, v2.x, v2.y)
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight)
+
+    cols = 1 + width / rez
+    rows = 1 + height / rez
+    for (let i = 0; i < cols; i++) {
+        let k = []
+        for (let j = 0; j < rows; j++) {
+            k.push(0)
+        }
+        field.push(k)
+    }
+}
--- main.js
+++ main.js
@@ -1,46 +1,127 @@
-const categoriesText = ["your website.", "your e-shop.", "your app.", "Mixed Reality."]
+const categoriesText = ["your app.", "your website.", "mixed reality.", "Your idea."]
 
-let scrollState = 0;
+let scrollState = 0
+let waitingForAnimation = false
 
 let containerLogo = document.getElementById("container-logo")
 let logo = document.getElementById("logo-main")
 let categories = document.getElementById("categories-text")
 
-document.addEventListener("wheel", function (event) {
-    const direction = event.deltaY * 0.01
+let startX, startY
+let startTime
 
-    scrollState = Math.min(Math.max(0, scrollState + direction), 10);
+document.addEventListener("wheel", function (event) {
+
+    const direction = event.deltaY * 0.01
 
     console.log(scrollState)
 
-    switch (scrollState) {
-        case 0:
-            
-            containerLogo.animate([{ top: "30px" }], { duration: 900, fill: "forwards", easing: "ease" })
-            containerLogo.animate([{ fontSize: "6em" }], { duration: 900, fill: "forwards", easing: "ease" })
-            categories.animate([{opacity:0}], { duration: 300, fill: "forwards", easing: "ease" })
+    advanceWebpage(direction)
+})
 
-            setTimeout(() => {
-                if (scrollState == 0) {
-                    categories.textContent = ""
-                    logo.textContent = "ncode."
-                }
-            }, 800)
-            break;
-        case 1:
-            logo.textContent = "ncode"
-            containerLogo.animate([{ top: "40vh" }], { duration: 900, fill: "forwards", easing: "ease" })
-            containerLogo.animate([{ fontSize: "10em" }], { duration: 900, fill: "forwards", easing: "ease" })
-            
+document.addEventListener("touchstart", (event) => {
+    startX = event.touches[0].pageX
+    startY = event.touches[0].pageY
+    startTime = event.timeStamp
+})
 
-            setTimeout(() => {
-                if (scrollState == 1) {
-                    categories.textContent = categoriesText[0]
-                    categories.animate([{opacity:1}], { duration: 300, fill: "forwards", easing: "ease" })
-                }
-            }, 800)
+document.addEventListener("touchend", (event) => {
+    console.log(event)
+    let endX = event.changedTouches[0].pageX
+    let endY = event.changedTouches[0].pageY
+    let endTime = event.timeStamp
 
-        default:
-            break;
+    let duration = endTime - startTime
+
+    let directionX = endX - startX
+    let directionY = endY - startY
+
+    let treshold = 50
+
+    if (duration < 500) {
+        if (directionY < -treshold) {
+            advanceWebpage(1)
+        }
+        if (directionY > treshold) {
+            advanceWebpage(-1)
+        }
     }
 })
+
+let arrow = document.getElementById("arrow")
+
+arrow.addEventListener("click", () => {
+    advanceWebpage(1)
+})
+
+function advanceWebpage(direction) {
+    if (waitingForAnimation == false) {
+        scrollState = Math.min(Math.max(0, scrollState + direction), 10);
+        switch (scrollState) {
+
+            case 0:
+
+                containerLogo.animate([{ top: "30px" }], { duration: 700, fill: "forwards", easing: "ease" })
+                containerLogo.animate([{ fontSize: "calc(min(16vw,110px))" }], { duration: 700, fill: "forwards", easing: "ease" })
+                categories.animate([{ opacity: 0 }], { duration: 300, fill: "forwards", easing: "ease" })
+
+                setTimeout(() => {
+                    if (scrollState == 0) {
+                        categories.textContent = ""
+                        logo.textContent = "ncode."
+                    }
+                }, 400)
+
+                break
+            case 1:
+
+                logo.textContent = "ncode"
+                containerLogo.animate([{ top: "40vh" }], { duration: 700, fill: "forwards", easing: "ease" })
+                containerLogo.animate([{ fontSize: "calc(min(24vw,160px))" }], { duration: 700, fill: "forwards", easing: "ease" })
+
+                if (direction > 0) {
+                    waitingForAnimation = true
+                    setTimeout(() => {
+                        animateElementVerticalSwipe(categories, direction, categoriesText[0])
+                    }, 100)
+                } else {
+                    animateElementVerticalSwipe(categories, direction, categoriesText[0])
+                }
+
+                break
+            case 2:
+
+                animateElementVerticalSwipe(categories, direction, categoriesText[1])
+                break
+            case 3:
+                animateElementVerticalSwipe(categories, direction, categoriesText[2])
+                break
+            case 4:
+                animateElementVerticalSwipe(categories, direction, categoriesText[3])
+                break
+            default:
+
+
+                /*
+                if (scrollState > 5) {
+                    document.body.style.overflowY = "auto"
+                    document.body.style.height = "auto"
+                } else {
+                    document.body.style.overflowY = "hidden"
+                    document.body.style.height = "100%"
+                }*/
+                break;
+        }
+    }
+}
+
+function animateElementVerticalSwipe(element, direction, text) {
+    waitingForAnimation = true
+    element.animate([{ top: "0em" }, { top: -direction + "em" }, { top: direction + "em" }, { top: "0em" }], { duration: 400, fill: "forwards", easing: "ease" })
+    element.animate([{ opacity: 1 }, { opacity: -1 }, { opacity: -1 }, { opacity: -1 }, { opacity: 1 }], { duration: 400, fill: "forwards", easing: "linear" })
+
+    setTimeout(() => {
+        element.textContent = text
+        waitingForAnimation = false
+    }, 200)
+}
